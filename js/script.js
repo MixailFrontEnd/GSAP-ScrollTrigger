654672472c18672bb015f3255065bc09d2b11a84
Mykhailo <153931034+MixailFrontEnd@users.noreply.github.com>
2023-12-15 17:35:54 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const lenis = new Lenis({
+        duration: 1.2,
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+        lenis.raf(time * 1000);
+    });
+
+    const workInfoItems = document.querySelectorAll('.work__photo-item');
+    workInfoItems.forEach((item, index) => {
+        item.style.zIndex = workInfoItems.length - index;
+    });
+
+    gsap.set('.work__photo-item', {
+        clipPath: function () {
+            return 'inset(0px 0px 0px 0px)';
+        },
+    });
+
+    const animation = gsap.to('.work__photo-item:not(:last-child)', {
+        clipPath: function () {
+            return 'inset(0px 0px 100% 0px)';
+        },
+        stagger: 0.5,
+        ease: 'none',
+    });
+
+    ScrollTrigger.create({
+        trigger: '.work',
+        start: 'top top',
+        end: 'bottom bottom',
+        animation: animation,
+        scrub: 1,
+    });
+});
